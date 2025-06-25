c7f32a17a860c2625ade153fff3f25091d5696b7
ljk28 <ljk28@hotmail.com>
2025-06-25 15:27:10 +0100

    add operator function to check correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,22 @@ function checkAnswer () {
 
 }
 
+/**
+ * Gets the operands (numbers) and the operator (plus, minus etc)
+ * directly from the DOM, and return the correct answer
+ */
 
 function calculateCorrectAnswer () {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
