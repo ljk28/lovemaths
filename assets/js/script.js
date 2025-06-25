64dac6f78a3c6c63dc165c56f7bfca88cb13ce4d
ljk28 <ljk28@hotmail.com>
2025-06-25 18:29:16 +0100

    add subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,10 @@ function runGame(gameType) {
     displayAdditionQuestion(num1, num2);
    } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num1);
-   } else {
+   } else if (gameType === "subtract") {
+    dislplaySubtractQuestion(num1, num2);
+   } 
+    else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`; 
    }
@@ -73,7 +76,10 @@ function calculateCorrectAnswer () {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else {
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    }
+        else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -104,7 +110,10 @@ function displayAdditionQuestion (operand1, operand2) {
 
 }
 
-function dislplaySubtractQuestion () {
+function dislplaySubtractQuestion (operand1, operand2) {
+    document.getElementById('operand1') .textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2') .textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator') .textContent = "-";
 
 }
 
